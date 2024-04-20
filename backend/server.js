6281281119178603d070cf36d565b52cffd429d9
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-20 21:45:13 +0600

    custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -12,10 +12,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+
+
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
