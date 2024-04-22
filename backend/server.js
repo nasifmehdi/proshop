406ca3e639cc66ae589f3e420e4c1125aaaf38cf
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-22 23:15:44 +0600

    user routes and cotrolllers

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 
+import userRoutes from './routes/userRoutes.js';
+
 connectDB();
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -20,7 +22,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
