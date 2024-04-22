82d62bd83eefe01fdc034f52d65a48085934d1ff
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-22 23:46:42 +0600

    user auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
@@ -23,6 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
