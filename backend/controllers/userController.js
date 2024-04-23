15e9964687fc589b161b478ca7a541eea49ef8fa
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-23 19:17:19 +0600

    logout user clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send('logout user');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
+  //res.send('logout user');
 };
 
 // @desc    Get user profile
