eb9a81164bb3e9d821d1cf3b4233f0e6970b68c6
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-25 19:12:41 +0600

    private routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import { Provider } from 'react-redux';
 import store from './store';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +28,9 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />;{' '}
-      <Route path='/shipping' element={<ShippingScreen />} />;
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
