7128e715c3d891ee9738938ac5fb7f9c951e3983
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-29 19:12:44 +0600

    Upload Product Image - Backend

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,35 @@ import productRoutes from './routes/productRoutes.js';
 
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 import cookieparser from 'cookie-parser';
+import path from 'path';
 
 connectDB();
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(cookieparser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
@@ -28,6 +42,7 @@ app.get('/api/config/paypal', (req, res) =>
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 
 app.use(notFound);
