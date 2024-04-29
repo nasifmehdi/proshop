7047358406444606d1b02fa691e67f57c5c9e262
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-29 18:46:34 +0600

    creating products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,13 +5,16 @@ import Product from '../models/productModel.js';
 import {
   getProducts,
   getProductById,
+  createProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 //const express = require('express');
 const router = express.Router();
 
 
-router.route('/').get(getProducts);
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router;
