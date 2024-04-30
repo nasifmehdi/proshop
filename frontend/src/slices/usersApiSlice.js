fc5d0b785afb4d0ba9a9a0e2ce27b1c3ecef8975
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2024-04-30 18:54:28 +0600

    delete users

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -37,8 +37,14 @@ export const usersApiSlice = apiSlice.injectEndpoints({
       providesTags: ['User'],
       keepUnusedDataFor: 5,
     }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: 'DELETE',
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation,  useProfileMutation,  useGetUsersQuery,} =
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation,  useProfileMutation,  useGetUsersQuery,  useDeleteUserMutation,} =
   usersApiSlice;
